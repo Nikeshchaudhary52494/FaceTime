3e5799060efae0b5967078c649ea37c94a9a2b4d
Nikesh Chaudhary <nikeshchaudhary52494@gmail.com>
2024-11-27 23:37:33 +0530

    config file updated

--- frontend/config.js
+++ frontend/config.js
@@ -1,3 +1,6 @@
-const mode = "dev"
+const mode = process.env.NODE_ENV === "production" ? "prod" : "dev";
 
-export const SERVER_URL = mode == "dev" ? 'ws://localhost:5000/ws' : "https://facetime-32ow.onrender.com"
+export const SERVER_URL = {
+    dev: 'ws://localhost:5000/ws',
+    prod: 'https://facetime-32ow.onrender.com'
+}[mode];
